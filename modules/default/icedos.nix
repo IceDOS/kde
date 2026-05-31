e2fb6735079781f10142a53f3bd2bebfa9fd4664
CrazyStevenz <github.ekta@aleeas.com>
2026-05-31 21:14:35 +0300

    Add build cache, excluded packages

--- modules/default/icedos.nix
+++ modules/default/icedos.nix
@@ -1,12 +1,39 @@
 { ... }:
 
 {
+  inputs.plasma67-cache.url = "github:IceDBorn/plasma67-cache";
+
   outputs.nixosModules =
     { ... }:
     [
-      {
-        services.desktopManager.plasma6.enable = true;
-      }
+      (
+        {
+          pkgs,
+          ...
+        }:
+        {
+          nix.settings = {
+            substituters = [ "https://cache.garnix.io" ];
+            trusted-public-keys = [
+              "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+            ];
+          };
+
+          services.desktopManager.plasma6.enable = true;
+          environment.plasma6.excludePackages = with pkgs.kdePackages; [
+            discover # Flatpak search
+            dolphin # File manager
+            elisa # Music player
+            gwenview # Image viewer
+            kate # Advanced text editor
+            khelpcenter # Help center
+            konsole # Terminal
+            ktexteditor # KTextEditor Framework
+            milou # Dedicated search application built on top of Baloo
+            okular # Document viewer
+          ];
+        }
+      )
     ];
 
   meta = {
